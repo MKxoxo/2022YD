2566b7c0134007db28b38d1ad39222b392f4a8ad
MKxoxo <zicho.ahah@gmail.com>
2022-08-03 11:22:57 +0900

    additional info

--- GOAT.py
+++ GOAT.py
@@ -1,3 +1,3 @@
 #edited by vi
 print("life too short, use python") 
-
+###################################
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ personal_yeardream_2022
 
 description:
 yeardreamschool 2022에서 개인적으로 코딩관련한 공부
+-코딩테스트
